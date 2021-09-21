d34aac25e1edd01d372fba9c34976db8a385122d
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-09-21 14:23:24 +0900

    Create Solution.java

---
+++ Level1/소수 만들기/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int sum = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				for (int k = j + 1; k < nums.length; k++) {
+					sum = nums[i] + nums[j] + nums[k];
+
+					if (isPrime(sum)) {
+						answer++;
+					}
+
+				}
+			}
+		}
+        return answer;
+    }
+    public static boolean isPrime(int n) {
+		boolean prime = true;
+
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			if (n % i == 0) {
+				prime = false;
+			}
+		}
+		return prime;
+	}
+}
