2714229c25b721452b4b4bbccbb99b32338bdc45
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-09-26 23:14:44 +0900

    Update Solution.java

--- Level1/약수의 개수와 덧셈/Solution.java
+++ Level1/약수의 개수와 덧셈/Solution.java
@@ -1,22 +1,22 @@
 class Solution {
     public int solution(int left, int right) {
-        int answer = 0;
+      int answer = 0;
         
       for (int i = left; i <= right; i++) {
-			  int count = 0; // 약수의 개수			
-			  for (int j = 1; j <= i; j++) {
+	int count = 0; // 약수의 개수			
+	for (int j = 1; j <= i; j++) {
 
-				if (i % j == 0) {
-					count++;
-				}
-			}
+	if (i % j == 0) {
+		count++;
+	       }
+	}
 
-			if (count % 2 == 0) {
-				answer += i;
-			} else {
-				answer -= i;
-			}
-		}        
+	if (count % 2 == 0) {
+		answer += i;
+	} else {
+		answer -= i;
+	}
+    }	        
         return answer;
-    }
+  }
 }
