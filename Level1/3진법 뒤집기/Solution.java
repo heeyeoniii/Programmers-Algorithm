f07c4350f227f5598916fbaaac9ce078de98d68d
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-09-26 23:40:13 +0900

    Update Solution.java

--- Level1/3진법 뒤집기/Solution.java
+++ Level1/3진법 뒤집기/Solution.java
@@ -4,16 +4,16 @@ class Solution {
         String num3 = "";
         
         while (n > 0) {
-			    num3 += n % 3;
-			    n = n / 3;
-		}
+		num3 += n % 3;
+		n = n / 3;
+	}
 
-		int count = 1;
+	int count = 1;
 
-		for (int i = num3.length() - 1; i >= 0; i--) {
-			answer += (num3.charAt(i) - '0') * count;
-			count *= 3;
-		}       
+	for (int i = num3.length() - 1; i >= 0; i--) {
+		answer += (num3.charAt(i) - '0') * count;
+		count *= 3;
+	}       
         
         return answer;
     }
