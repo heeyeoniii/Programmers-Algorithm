322546ee626c104e767f8f93f0f19a6387c39989
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-09-26 23:15:23 +0900

    Update Solution.java

--- Level1/내적/Solution.java
+++ Level1/내적/Solution.java
@@ -3,8 +3,8 @@ class Solution {
         int answer = 0;
         
         for (int i = 0; i < a.length; i++) {
-			answer += a[i] * b[i];
-		}        
+		answer += a[i] * b[i];
+	}        
         return answer;
     }
 }
