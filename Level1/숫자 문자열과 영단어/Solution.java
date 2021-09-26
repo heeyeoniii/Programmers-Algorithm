754716fc1b29ea8a3c152498eb8c583ddf4a920a
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-09-26 23:18:05 +0900

    Update Solution.java

--- Level1/숫자 문자열과 영단어/Solution.java
+++ Level1/숫자 문자열과 영단어/Solution.java
@@ -1,25 +1,24 @@
 class Solution {
     public int solution(String s) {
-				// 방법 1.
+	// 방법 1.
         s = s.replaceAll("zero", "0");
-				s = s.replaceAll("one", "1");
-				s = s.replaceAll("two", "2");
-				s = s.replaceAll("three", "3");
-				s = s.replaceAll("four", "4");
-				s = s.replaceAll("five", "5");
-				s = s.replaceAll("six", "6");
-				s = s.replaceAll("seven", "7");
-				s = s.replaceAll("eight", "8");
-				s = s.replaceAll("nine", "9");  
+	s = s.replaceAll("one", "1");
+	s = s.replaceAll("two", "2");
+	s = s.replaceAll("three", "3");
+	s = s.replaceAll("four", "4");
+	s = s.replaceAll("five", "5");
+	s = s.replaceAll("six", "6");
+	s = s.replaceAll("seven", "7");
+	s = s.replaceAll("eight", "8");
+	s = s.replaceAll("nine", "9");  
 
-				// 방법 2. 배열 사용
-				String[] numArr = {"zero", "one", "two", "three", "four", "five",
+	// 방법 2. 배열 사용
+	String[] numArr = {"zero", "one", "two", "three", "four", "five",
 		           "six", "seven", "eight", "nine"};
 
-				for (int i = 0; i < numArr.length; i++) {
-						s = s.replace(numArr[i], Integer.toString(i));
-				}      
-        
+	for (int i = 0; i < numArr.length; i++) {
+		s = s.replace(numArr[i], Integer.toString(i));
+	}              
         int answer = Integer.parseInt(s);
         return answer;
     }
