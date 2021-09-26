7ba9a70d2d7d642ee8f83f460bc8be98a752c19f
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-09-26 23:19:16 +0900

    Update Solution.java

--- Level1/완주하지 못한 선수/Solution.java
+++ Level1/완주하지 못한 선수/Solution.java
@@ -5,30 +5,29 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         
-      	        // 배열 정렬
-		Arrays.sort(participant);
-		Arrays.sort(completion);
+      	// 배열 정렬
+	Arrays.sort(participant);
+	Arrays.sort(completion);
 
-		HashMap<String, Integer> map = new HashMap<>();
+	HashMap<String, Integer> map = new HashMap<>();
 
-		for (String p : participant) {
-			// key : 참가자 p
-			// value : +1
-			map.put(p, map.getOrDefault(p, 0) + 1);
+	for (String p : participant) {
+		// key : 참가자 p
+		// value : +1
+		map.put(p, map.getOrDefault(p, 0) + 1);
+	}
+
+	for (String c : completion) {
+		// 참가자 명단이 map에 존재하면, value : -1
+		map.put(c, map.getOrDefault(c, 0) - 1);
+	}
+
+	for (String key : map.keySet()) {
+		if (map.get(key) != 0) {
+			answer += key;
+			break;
 		}
-
-		for (String c : completion) {
-			// 참가자 명단이 map에 존재하면, value : -1
-			map.put(c, map.getOrDefault(c, 0) - 1);
-		}
-
-		for (String key : map.keySet()) {
-			if (map.get(key) != 0) {
-				answer += key;
-				break;
-			}
-		}
-        
+	}    
         return answer;
     }
 }
