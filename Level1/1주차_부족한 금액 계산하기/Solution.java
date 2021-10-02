215bd409a013ef71bad0cd39d76ac3f70625aaf6
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-02 23:36:35 +0900

    Create Solution.java

---
+++ Level1/1주차_부족한 금액 계산하기/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = -1;
+        long realPrice = 0; // long으로 선언하지 않을 시 에러 발생
+        int num = 1;
+        
+        for (int i = 0; i < count; i++) {
+			realPrice += num * price;
+			num++;
+			continue;
+		}
+        
+        answer = realPrice - money;
+        if(money > realPrice) {
+            answer = 0;
+        }
+
+        return answer;
+    }
+}
