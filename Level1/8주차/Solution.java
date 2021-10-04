306a863a7d2951ddc359bc46c279c2eded444b21
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-04 21:12:11 +0900

    Create Solution.java

---
+++ Level1/8주차/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int width = 0;
+		int height = 0;
+        
+        for (int i = 0; i < sizes.length; i++) {
+			if (sizes[i][0] < sizes[i][1]) {
+				int temp = sizes[i][1];
+				sizes[i][1] = sizes[i][0];
+				sizes[i][0] = temp;
+			}
+
+			if (width < sizes[i][0]) {
+				width = sizes[i][0];
+			}
+
+			if (height < sizes[i][1]) {
+				height = sizes[i][1];
+			}
+		}
+        
+        answer = width * height;
+        
+        return answer;
+    }
+}
