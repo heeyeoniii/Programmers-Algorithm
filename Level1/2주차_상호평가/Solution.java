5d885dd16b60657c8a02ddf99edaf585b80b7071
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-06 16:27:28 +0900

    Update Solution.java

--- Level1/2주차_상호평가/Solution.java
+++ Level1/2주차_상호평가/Solution.java
@@ -1,42 +1,48 @@
-class Solution {
-    public String solution(int[][] scores) {
-        String answer = "";
-        char grade;
-        
-        for (int i = 0; i < scores.length; i++) {
-	   	int sum = 0, del = 0, max = -1, min = 101;
-	  	double avg = 0;
-		for (int j = 0; j < scores.length; j++) {
+public class Solution {
 
-		if (i == j) continue;
+	public static void main(String[] args) {
 
-		min = Math.min(min, scores[j][i]);
-		max = Math.max(max, scores[j][i]);
-		sum += scores[j][i];
+		int[][] scores = { { 50, 90 }, { 50, 87 } };
 
-		if (scores[i][i] > max || scores[i][i] < min) {
-			avg = (double) sum / (scores.length - 1);
+		String answer = "";
+		char grade;
+
+		for (int i = 0; i < scores.length; i++) {
+			int sum = 0, max = 0, min = 100;
+			double avg = 0;
+			for (int j = 0; j < scores.length; j++) {
+
+				if (i == j) continue;
+
+				min = Math.min(min, scores[j][i]);
+				max = Math.max(max, scores[j][i]);
+				sum += scores[j][i];
+
+				if (scores[i][i] > max || scores[i][i] < min) {
+					avg = (double) sum / (scores.length - 1);
+				} else {
+
+					avg = (double) (sum + scores[i][i]) / scores.length;
+				}
+				System.out.println(avg);
+
+			}
+
+			if (avg >= 90) {
+				grade = 'A';
+			} else if (avg >= 80 && avg < 90) {
+				grade = 'B';
+			} else if (avg >= 70 && avg < 80) {
+				grade = 'C';
+			} else if (avg >= 50 && avg < 70) {
+				grade = 'D';
 			} else {
+				grade = 'F';
+			}
 
-			avg = (double) (sum + scores[i][i]) / scores.length;
+			answer += grade;
+
 		}
-
+		System.out.println(answer);
 	}
-
-	if (avg >= 90) {
-		grade = 'A';
-	} else if (avg >= 80 && avg < 90) {
-		grade = 'B';
-	} else if (avg >= 70 && avg < 80) {
-		grade = 'C';
-	} else if (avg >= 50 && avg < 70) {
-		grade = 'D';
-	} else {
-		grade = 'F';
-	}
-
-	answer += grade;
-        }    
-        return answer;
-    }
 }
