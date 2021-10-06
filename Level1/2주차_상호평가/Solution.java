51858c28776049f00e3181f9dde47f5f35a25367
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-06 16:25:18 +0900

    Update Solution.java

--- Level1/2주차_상호평가/Solution.java
+++ Level1/2주차_상호평가/Solution.java
@@ -4,39 +4,38 @@ class Solution {
         char grade;
         
         for (int i = 0; i < scores.length; i++) {
-			int sum = 0, del = 0, max = -1, min = 101;
-            int maxCount = 0, minCount = 0;
-			double avg = 0;
-			for (int j = 0; j < scores.length; j++) {
+	   	int sum = 0, del = 0, max = -1, min = 101;
+	  	double avg = 0;
+		for (int j = 0; j < scores.length; j++) {
 
-				if (i == j) continue;
+		if (i == j) continue;
 
-				min = Math.min(min, scores[j][i]);
-				max = Math.max(max, scores[j][i]);
-				sum += scores[j][i];
+		min = Math.min(min, scores[j][i]);
+		max = Math.max(max, scores[j][i]);
+		sum += scores[j][i];
 
-				if (scores[i][i] > max || scores[i][i] < min) {
-					avg = (double) sum / (scores.length - 1);
-				} else {
-
-					avg = (double) (sum + scores[i][i]) / scores.length;
-				}
-
-			}
-
-			if (avg >= 90) {
-				grade = 'A';
-			} else if (avg >= 80 && avg < 90) {
-				grade = 'B';
-			} else if (avg >= 70 && avg < 80) {
-				grade = 'C';
-			} else if (avg >= 50 && avg < 70) {
-				grade = 'D';
+		if (scores[i][i] > max || scores[i][i] < min) {
+			avg = (double) sum / (scores.length - 1);
 			} else {
-				grade = 'F';
-			}
 
-			answer += grade;
+			avg = (double) (sum + scores[i][i]) / scores.length;
+		}
+
+	}
+
+	if (avg >= 90) {
+		grade = 'A';
+	} else if (avg >= 80 && avg < 90) {
+		grade = 'B';
+	} else if (avg >= 70 && avg < 80) {
+		grade = 'C';
+	} else if (avg >= 50 && avg < 70) {
+		grade = 'D';
+	} else {
+		grade = 'F';
+	}
+
+	answer += grade;
         }    
         return answer;
     }
