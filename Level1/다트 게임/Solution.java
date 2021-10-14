06c56149470df6a839c425448138f335a72a9498
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-14 23:16:55 +0900

    Update Solution.java

--- Level1/다트 게임/Solution.java
+++ Level1/다트 게임/Solution.java
@@ -1,62 +1,55 @@
-package codingTest;
-
 import java.util.Stack;
 
-public class Solution {
+class Solution {
+    public int solution(String dartResult) {
+        
+        Stack<Integer> stack = new Stack<>();
+        int answer = 0;
 
-	public static void main(String[] args) {
+	for (int i = 0; i < dartResult.length(); i++) {
+		char c = dartResult.charAt(i);
 
-		String dartResult = "1S2D*3T";
-
-		Stack<Integer> stack = new Stack<>();
-		int answer = 0;
-
-		for (int i = 0; i < dartResult.length(); i++) {
-			char c = dartResult.charAt(i);
-
-			if (c == 'S') {
-				stack.add((int) Math.pow(stack.pop(), 1));
-				continue;
-			}
-
-			if (c == 'D') {
-				stack.add((int) Math.pow(stack.pop(), 2));
-				continue;
-			}
-
-			if (c == 'T') {
-				stack.add((int) Math.pow(stack.pop(), 3));
-				continue;
-			}
-
-			if (c == '*') {
-				int num = stack.pop();
-				if (!stack.isEmpty()) {
-					stack.add(stack.pop() * 2);
-				}
-				stack.add(num * 2);
-				continue;
-			}
-
-			if (c == '#') {
-				stack.add(stack.pop() * -1);
-				continue;
-			}
-
-			if (c - '0' == 1 && dartResult.charAt(i + 1) == '0') {
-				stack.add(10);
-				i++;
-
-			} else {
-				stack.add(c - '0');
-			}
+		if (c == 'S') {
+			stack.add((int) Math.pow(stack.pop(), 1));
+			continue;
 		}
 
-		for (int score : stack) {
-			answer += score;
+		if (c == 'D') {
+			stack.add((int) Math.pow(stack.pop(), 2));
+			continue;
 		}
 
-		System.out.println(answer);
+		if (c == 'T') {
+			stack.add((int) Math.pow(stack.pop(), 3));
+			continue;
+		}
 
+		if (c == '*') {
+			int num = stack.pop();
+			if (!stack.isEmpty()) {
+				stack.add(stack.pop() * 2);
+			}
+			stack.add(num * 2);
+			continue;
+		}
+
+		if (c == '#') {
+			stack.add(stack.pop() * -1);
+			continue;
+		}
+
+		if (c - '0' == 1 && dartResult.charAt(i + 1) == '0') {
+			stack.add(10);
+			i++;
+
+		} else {
+			stack.add(c - '0');
+		}
 	}
+
+	for (int score : stack) {
+		answer += score;
+	}
+        return answer;
+    }
 }
