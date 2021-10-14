6fcd6c603ce5a8248b7fd367bff9b419ac32fb1a
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-14 23:17:44 +0900

    Update Solution.java

--- Level1/같은 숫자는 싫어/Solution.java
+++ Level1/같은 숫자는 싫어/Solution.java
@@ -1,29 +1,25 @@
+import java.util.*;
+
 public class Solution {
+    public int[] solution(int []arr) {
+        
+        List<Integer> list = new ArrayList<>();
 
-	public static void main(String[] args) {
+	for (int i = 0; i < arr.length - 1; i++) {
 
-		int arr[] = { 1, 1, 3, 3, 0, 1, 1 };
-
-		List<Integer> list = new ArrayList<>();
-
-		for (int i = 0; i < arr.length - 1; i++) {
-
-			if (arr[i] != arr[i + 1]) {
-				list.add(arr[i]);
-			}
+		if (arr[i] != arr[i + 1]) {
+			list.add(arr[i]);
 		}
-
-		list.add(arr[arr.length - 1]); // 마지막 인덱스의 값 추가
-
-		int answer[] = new int[list.size()];
-
-		for (int i = 0; i < list.size(); i++) {
-			answer[i] = list.get(i);
-		}
-
-		for (int num : list) {
-			System.out.println(num);
-		}
-
 	}
+
+	list.add(arr[arr.length - 1]); // 마지막 인덱스의 값 추가
+
+	int answer[] = new int[list.size()];
+
+	for (int i = 0; i < list.size(); i++) {
+		answer[i] = list.get(i);
+	}
+        
+        return answer;
+    }
 }
