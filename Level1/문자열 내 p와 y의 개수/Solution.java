be0fd36f15c79fe56a57508672bc1aabc303e6e0
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-14 23:13:37 +0900

    Update Solution.java

--- Level1/문자열 내 p와 y의 개수/Solution.java
+++ Level1/문자열 내 p와 y의 개수/Solution.java
@@ -2,21 +2,21 @@ class Solution {
     boolean solution(String s) {
         
         int countP = 0, countY = 0;
-		boolean answer = true;
+	boolean answer = true;
 
-		s = s.toUpperCase();
+	s = s.toUpperCase();
 
-		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) == 'P') {
-				countP++;
-			} else if (s.charAt(i) == 'Y') {
-				countY++;
-			}
+	for (int i = 0; i < s.length(); i++) {
+		if (s.charAt(i) == 'P') {
+			countP++;
+		} else if (s.charAt(i) == 'Y') {
+			countY++;
 		}
+	}
 
-		if(countP != countY) {
-			answer = false;
-		}
+	if(countP != countY) {
+		answer = false;
+	}
 
         return answer;
     }
