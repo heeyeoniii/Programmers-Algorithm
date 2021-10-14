38b6db26e0e1e6930523b9aec3f05058c73c9b1d
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-14 23:21:30 +0900

    Update Solution.java

--- Level1/1차 비밀지도/Solution.java
+++ Level1/1차 비밀지도/Solution.java
@@ -1,22 +1,13 @@
-public class Solution {
-
-	public static void main(String[] args) {
-
-		int n = 5;
-		int[] arr1 = { 9, 20, 28, 18, 11 };
-		int[] arr2 = { 30, 1, 21, 17, 28 };
-
-		String[] answer = new String[n];
-
-		for (int i = 0; i < n; i++) {
-			answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
-			answer[i] = String.format("%" + n + "s", answer[i]);
-			answer[i] = answer[i].replace("1", "#");
-			answer[i] = answer[i].replace("0", " ");
-
-			System.out.println(answer[i]);
-		}
-
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];       
+        
+        for(int i=0; i<n; i++) {
+            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+	    answer[i] = String.format("%" + n + "s", answer[i]);
+	    answer[i] = answer[i].replace("1", "#");
+	    answer[i] = answer[i].replace("0", " ");			
 	}
-
+        return answer;
+    }
 }
