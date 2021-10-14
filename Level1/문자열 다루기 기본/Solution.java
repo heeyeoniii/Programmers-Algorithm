087096308d4edf22fa6e6d62bf9f0e063e71175b
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-14 23:05:14 +0900

    Update Solution.java

--- Level1/문자열 다루기 기본/Solution.java
+++ Level1/문자열 다루기 기본/Solution.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean solution(String s) {
 
-
-		if(s.length() == 4 || s.length() == 6) {
+	if(s.length() == 4 || s.length() == 6) {
             try {
                 int x = Integer.parseInt(s); // 문자열을 정수로 변환, 에러 발생 경우 false 리턴
                 return true;
+		    
             } catch(NumberFormatException e) {
                 return false;
             }
+		
         } else {
             return false;
         }
