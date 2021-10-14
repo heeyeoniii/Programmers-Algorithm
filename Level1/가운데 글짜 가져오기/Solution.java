1451ec0cbcc12f698cd85a0bd8ab9da769f5f7b9
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-14 23:18:59 +0900

    Update Solution.java

--- Level1/가운데 글짜 가져오기/Solution.java
+++ Level1/가운데 글짜 가져오기/Solution.java
@@ -1,20 +1,16 @@
-public class Solution {
-
-	public static void main(String[] args) {
-
-		String s = "abcde";
-		String answer = "";
-
-		if (s.length() % 2 == 0) {
-			// 단어의 길이가 짝수일 경우
-			answer += s.charAt((s.length() / 2) - 1);
-			answer += s.charAt(s.length() / 2);
-		} else {
-			// 단어의 길이가 홀수일 경우
-			answer += s.charAt(s.length() / 2);
-		}
-
-		System.out.println(answer);
-
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+        if (s.length() % 2 == 0) {
+		// 단어의 길이가 짝수일 경우
+		answer += s.charAt((s.length() / 2) - 1);
+		answer += s.charAt(s.length() / 2);
+	} else {
+		// 단어의 길이가 홀수일 경우
+		answer += s.charAt(s.length() / 2);
 	}
+        
+        return answer;
+    }
 }
