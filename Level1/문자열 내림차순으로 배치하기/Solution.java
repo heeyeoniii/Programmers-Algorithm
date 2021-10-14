b6cd8c0c4cc3b6654c6204460b9a3e11d64ffcc1
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-14 23:14:39 +0900

    Update Solution.java

--- Level1/문자열 내림차순으로 배치하기/Solution.java
+++ Level1/문자열 내림차순으로 배치하기/Solution.java
@@ -1,23 +1,20 @@
-public class Solution {
+import java.util.Arrays;
 
-	public static void main(String[] args) {
+class Solution {
+    public String solution(String s) {
+        char[] str = new char[s.length()];
+	String answer = "";
 
-		String s = "Zbcdefg";
-
-		char[] str = new char[s.length()];
-		String answer = "";
-
-		for (int i = 0; i < s.length(); i++) {
-			str[i] = s.charAt(i);
-		}
-
-		Arrays.sort(str);
-
-		for (int i = str.length - 1; i >= 0; i--) {
-			answer += str[i];
-		}
-
-		System.out.println(answer);
+	for (int i = 0; i < s.length(); i++) {
+		str[i] = s.charAt(i);
 	}
 
+	Arrays.sort(str);
+
+	for (int i = str.length - 1; i >= 0; i--) {
+		answer += str[i];
+	}
+        
+        return answer;
+    }
 }
