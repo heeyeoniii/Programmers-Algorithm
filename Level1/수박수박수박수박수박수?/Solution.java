633f742d785a20effbb341e0614d996160865f49
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-16 23:47:42 +0900

    Update Solution.java

--- Level1/수박수박수박수박수박수?/Solution.java
+++ Level1/수박수박수박수박수박수?/Solution.java
@@ -3,13 +3,12 @@ class Solution {
         String answer = "";
         
         for (int i = 1; i <= n; i++) {
-			if (i % 2 != 0) {
-				answer += "수";
-			} else {
-				answer += "박";
-			}
+		if (i % 2 != 0) {
+			answer += "수";
+		} else {
+			answer += "박";
 		}
-        
+	}        
         return answer;
     }
 }
