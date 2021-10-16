a30ac4748e3d8eacdfcd0b07d0bbbecd375ac84d
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-16 23:41:14 +0900

    Update Solution.java

--- Level1/소수 찾기/Solution.java
+++ Level1/소수 찾기/Solution.java
@@ -2,20 +2,18 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
 
-		for (int i = 2; i <= n; i++) {
-			int count = 0;
-
-			for (int j = 2; j <= Math.sqrt(i); j++) {
-				if (i % j == 0) {
-					count++;
-					break;
-				}
-			}
-			if (count == 0) {
-				answer++;
-			}
-
+	for (int i = 2; i <= n; i++) {
+		int count = 0;
+		for (int j = 2; j <= Math.sqrt(i); j++) {
+			if (i % j == 0) {
+				count++;
+				break;
+			}	
 		}
+		if (count == 0) {
+			answer++;
+		}
+	}
         return answer;
     }
 }
