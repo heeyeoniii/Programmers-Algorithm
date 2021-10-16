268a56f6caae0cd9fdc7e890bc06008e7b1213d2
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-16 23:06:11 +0900

    Update Solution.java

--- Level1/서울에서 김서방 찾기/Solution.java
+++ Level1/서울에서 김서방 찾기/Solution.java
@@ -3,11 +3,11 @@ class Solution {
         String answer = "";
         int count = 0;
         
-      for (int i = 0; i < seoul.length; i++) {
-			if (seoul[i].equals("Kim")) {
-				count = i;
-			}
+        for (int i = 0; i < seoul.length; i++) {
+		if (seoul[i].equals("Kim")) {
+			count = i;
 		}
+	}
         answer = "김서방은 " + count + "에 있다";        
         return answer;
     }
