7089b7149d3f6ffdfc129b47dd9b50c2cfd64e82
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-17 23:47:38 +0900

    Update Solution.java

--- Level1/시저 암호/Solution.java
+++ Level1/시저 암호/Solution.java
@@ -3,19 +3,18 @@ class Solution {
         String answer = "";
         
         for (int i = 0; i < s.length(); i++) {
-			  char c = s.charAt(i);
+		  char c = s.charAt(i);
 
-			  if (Character.isLowerCase(c)) {
-				  // 소문자라면
-				  c = (char) ((c - 'a' + n) % 26 + 'a'); // 알파벳의 개수 : 26개
+		  if (Character.isLowerCase(c)) {
+			  // 소문자라면
+			  c = (char) ((c - 'a' + n) % 26 + 'a'); // 알파벳의 개수 : 26개
 
-			  } else if (Character.isUpperCase(c)) {
-				  // 대문자라면
-				  c = (char) ((c - 'A' + n) % 26 + 'A');
-			  }
-			  answer += c;
+		  } else if (Character.isUpperCase(c)) {
+			  // 대문자라면
+			  c = (char) ((c - 'A' + n) % 26 + 'A');
 		  }
-        
+		  answer += c;
+	  } 
         return answer;
     }
 }
