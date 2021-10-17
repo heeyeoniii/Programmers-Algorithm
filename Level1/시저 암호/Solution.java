4431c852c7040bca8074ebd678b64f30e244af86
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-17 23:46:47 +0900

    Create Solution.java

---
+++ Level1/시저 암호/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String s, int n) {
+        String answer = "";
+        
+        for (int i = 0; i < s.length(); i++) {
+			  char c = s.charAt(i);
+
+			  if (Character.isLowerCase(c)) {
+				  // 소문자라면
+				  c = (char) ((c - 'a' + n) % 26 + 'a'); // 알파벳의 개수 : 26개
+
+			  } else if (Character.isUpperCase(c)) {
+				  // 대문자라면
+				  c = (char) ((c - 'A' + n) % 26 + 'A');
+			  }
+			  answer += c;
+		  }
+        
+        return answer;
+    }
+}
