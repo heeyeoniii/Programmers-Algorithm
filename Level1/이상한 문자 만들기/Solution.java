47e6138b9e97d459c48c29111f64ff1bcfdcc511
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-18 15:38:01 +0900

    Update Solution.java

--- Level1/이상한 문자 만들기/Solution.java
+++ Level1/이상한 문자 만들기/Solution.java
@@ -4,22 +4,22 @@ class Solution {
         
         String[] str = s.split("");
 
-		int idx = 0;
+	int idx = 0;
 
-		for (int i = 0; i < str.length; i++) {
-			if (str[i].equals(" ")) {
-				idx = 0;
+	for (int i = 0; i < str.length; i++) {
+		if (str[i].equals(" ")) {
+			idx = 0;
 				
-			} else if (idx % 2 == 0) {
-				str[i] = str[i].toUpperCase();
-				idx++;
+		} else if (idx % 2 == 0) {
+			str[i] = str[i].toUpperCase();
+			idx++;
 				
-			} else if (idx % 2 != 0) {
-				str[i] = str[i].toLowerCase();
-				idx++;
-			}
-			answer += str[i];
+		} else if (idx % 2 != 0) {
+			str[i] = str[i].toLowerCase();
+			idx++;
 		}
+		answer += str[i];
+	}
         
         return answer;
     }
