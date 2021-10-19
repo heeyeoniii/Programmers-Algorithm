11755bcc4c1799a0ffe1a5e9a6a5add8db8cf423
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-19 21:44:14 +0900

    Create Solution.java

---
+++ Level1/정수 내림차순으로 배치하기/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        
+        String str = String.valueOf(n);
+        
+        Integer[] arr = new Integer[str.length()];
+
+		for (int i = 0; i < str.length(); i++) {
+			arr[i] = str.charAt(i) - '0';
+		}
+
+		Arrays.sort(arr, Collections.reverseOrder());
+
+		for (int i = 0; i < arr.length; i++) {
+			answer += arr[i] * Math.pow(10, arr.length - 1 - i);
+		}
+        
+        return answer;
+    }
+}
