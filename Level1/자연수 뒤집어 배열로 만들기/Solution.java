f0bcc224eb9e25ed803c30296881d763d376f387
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-19 21:27:33 +0900

    Update Solution.java

--- Level1/자연수 뒤집어 배열로 만들기/Solution.java
+++ Level1/자연수 뒤집어 배열로 만들기/Solution.java
@@ -3,9 +3,9 @@ class Solution {
         String str = String.valueOf(n);
         int[] answer = new int[str.length()];
 
-		for (int i = 0; i < answer.length; i++) {
-			answer[i] = str.charAt(answer.length - 1 - i) - '0';
-		}
+	for (int i = 0; i < answer.length; i++) {
+		answer[i] = str.charAt(answer.length - 1 - i) - '0';
+	}
         
         return answer;
     }
