b964e7dba85d854d4480948fa5d4a8d2ee338814
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-19 21:44:59 +0900

    Update Solution.java

--- Level1/정수 내림차순으로 배치하기/Solution.java
+++ Level1/정수 내림차순으로 배치하기/Solution.java
@@ -9,15 +9,15 @@ class Solution {
         
         Integer[] arr = new Integer[str.length()];
 
-		for (int i = 0; i < str.length(); i++) {
-			arr[i] = str.charAt(i) - '0';
-		}
+	for (int i = 0; i < str.length(); i++) {
+		arr[i] = str.charAt(i) - '0';
+	}
 
-		Arrays.sort(arr, Collections.reverseOrder());
+	Arrays.sort(arr, Collections.reverseOrder());
 
-		for (int i = 0; i < arr.length; i++) {
-			answer += arr[i] * Math.pow(10, arr.length - 1 - i);
-		}
+	for (int i = 0; i < arr.length; i++) {
+		answer += arr[i] * Math.pow(10, arr.length - 1 - i);
+	}
         
         return answer;
     }
