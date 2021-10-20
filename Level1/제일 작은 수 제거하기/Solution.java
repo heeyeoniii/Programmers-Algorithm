afefcbb8595a40f2943acbb867c7c93a2cada9b1
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-20 22:15:36 +0900

    Create Solution.java

---
+++ Level1/제일 작은 수 제거하기/Solution.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        if (arr.length <= 1) {
+			int[] answer = { -1 };
+			return answer;
+		}
+
+		int min = arr[0];
+
+		for (int i = 1; i < arr.length; i++) {
+			min = Math.min(min, arr[i]);
+		}
+
+		int[] answer = new int[arr.length - 1];
+		int idx = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			
+			if (arr[i] == min) {
+				continue;
+			}
+			answer[idx++] = arr[i];
+		}
+        
+        return answer;
+    }
+}
