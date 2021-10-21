b6944ffcc41afb922f0b786639297b56f2135ce8
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-21 16:24:15 +0900

    Update Solution.java

--- Level1/최대공약수와 최소공배수/Solution.java
+++ Level1/최대공약수와 최소공배수/Solution.java
@@ -3,23 +3,23 @@ class Solution {
         
         int[] answer = new int[2];
 
-		int min = Math.min(n, m);
-		int max = Math.max(n, m);
+	int min = Math.min(n, m);
+	int max = Math.max(n, m);
 
-		// 최대공약수
-		for (int i = 1; i <= min; i++) {
-			if (min % i == 0 && max % i == 0) {
-				answer[0] = i;
-			}
+	// 최대공약수
+	for (int i = 1; i <= min; i++) {
+		if (min % i == 0 && max % i == 0) {
+			answer[0] = i;
 		}
+	}
 
-		// 최소공배수
-		for (int i = min; i <= max * min; i++) {
-			if (i % min == 0 && i % max == 0) {
-				answer[1] = i;
-				break;
-			}
+	// 최소공배수
+	for (int i = min; i <= max * min; i++) {
+		if (i % min == 0 && i % max == 0) {
+			answer[1] = i;
+			break;
 		}
+	}
         
         return answer;
     }
