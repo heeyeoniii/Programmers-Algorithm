76b9fcf990f1bcdd78f941774d94f304baf6d828
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-24 19:10:08 +0900

    Update Solution.java

--- Level1/콜라츠 추측/Solution.java
+++ Level1/콜라츠 추측/Solution.java
@@ -1,25 +1,24 @@
 class Solution {
     public int solution(int n) {
-		long num = n;
-		int answer = 0;
+	long num = n;
+	int answer = 0;
 
-		while (num != 1) {
+	while (num != 1) {
 
-			if (num % 2 == 0) {
-				num /= 2;
+		if (num % 2 == 0) {
+			num /= 2;
 
-			} else {
-				num = num * 3 + 1;
-			}
-
-			answer++;
-
-			if (answer > 500) {
-				answer = -1;
-				break;
-			}
-
+		} else {
+			num = num * 3 + 1;
 		}
+
+		answer++;
+
+		if (answer > 500) {
+			answer = -1;
+			break;
+		}
+	}
         
         return answer;
     }
