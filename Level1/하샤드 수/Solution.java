873dcb7a757271f8a87e82eb136775e0443ee85a
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-25 15:45:14 +0900

    Create Solution.java

---
+++ Level1/하샤드 수/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean solution(int x) {
+        
+        String str = String.valueOf(x);
+
+		int sum = 0;
+		boolean answer = true;
+
+		for (int i = 0; i < str.length(); i++) {
+			sum += str.charAt(i) - '0';
+		}
+
+		if (x % sum != 0) {
+			answer = false;
+		}
+        
+        return answer;
+    }
+}
