b0fbcf58b82169c53a26df8a0fbd14137bfe98f7
heeyeoniii <90899286+heeyeoniii@users.noreply.github.com>
2021-10-26 15:37:10 +0900

    Update Solution.java

--- Level1/행렬의 덧셈/Solution.java
+++ Level1/행렬의 덧셈/Solution.java
@@ -3,11 +3,11 @@ class Solution {
         int answer[][] = new int[arr1.length][arr2[0].length];
 
 		
-		for (int i = 0; i < arr1.length; i++) {
-			for (int j = 0; j < arr2[0].length; j++) {
-				answer[i][j] = arr1[i][j] + arr2[i][j];
-			}
+	for (int i = 0; i < arr1.length; i++) {
+		for (int j = 0; j < arr2[0].length; j++) {
+			answer[i][j] = arr1[i][j] + arr2[i][j];
 		}
+	}
         
         return answer;
     }
