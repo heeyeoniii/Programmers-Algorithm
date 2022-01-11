f746da2f1248826c0a1ab871ddbe2e4040448b45
Heeyeon Cho <90899286+heeyeoniii@users.noreply.github.com>
2022-01-12 00:12:35 +0900

    Update Solution.java

--- Level2/최댓값과 최솟값/Solution.java
+++ Level2/최댓값과 최솟값/Solution.java
@@ -4,21 +4,23 @@ class Solution {
         
         String[] arr = s.split("\\s");
 
-		    int max = Integer.MIN_VALUE;
-	    	int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
 
-		    for (String str : arr) {
-			      int num = Integer.valueOf(str);
+		for (String str : arr) {
+			int num = Integer.valueOf(str);
 
-			      if (num > max) {
-				      max = num;
-			      }
+			if (num > max) {
+				max = num;
+			}
 
-			      if (num < min) {
-				      min = num;
-			      }
-		      }     
-        answer = min + " " + max;     
+			if (num < min) {
+				min = num;
+			}
+		}
+        
+        answer = min + " " + max;
+        
         return answer;
     }
 }
